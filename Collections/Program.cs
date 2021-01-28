0041b3751f1b56a050c3a28c1ddc45bf480dfa9c
Mert AKKAYA <mertakkaya522@gmail.com>
2021-01-28 18:29:23 +0300

    28 ocak tekrar

---
+++ Collections/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> isimler = new List<string> {"mert","berat","akın" };
+            Console.WriteLine(isimler[0]);
+            Console.WriteLine(isimler[1]);
+            Console.WriteLine(isimler[2]);
+            isimler.Add("cumali");
+            Console.WriteLine(isimler[3]);
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product product1 = new Product();
+            product1.Id = 1;
+            product1.CategoryId = 2;
+            product1.ProductName = "Masa";
+            product1.UnitPrice = 500;
+            product1.UnitsInStock = 5;
+
+            Product product2 = new Product
+            {
+                Id = 1,
+                CategoryId = 5,
+                ProductName = "Kalem",
+                UnitPrice = 35,
+                UnitsInStock = 10
+            };
+
+            ProductManager productManager = new ProductManager();
+            productManager.Add(product1);
+        }
+    }
+}
